5e3a4064a5fbc2a58762b971e76e02852b73e601
DJ Chemik <dj-chemik@wp.pl>
2020-03-09 22:22:50 +0100

    Finish (almost) all exercises

--- Lab2/MetadataLab2/src/Exercise2.java
+++ Lab2/MetadataLab2/src/Exercise2.java
@@ -1,22 +1,27 @@
+import java.awt.*;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 import org.apache.james.mime4j.internal.ParserStreamContentHandler;
 import org.apache.james.mime4j.parser.ContentHandler;
+import org.apache.tika.Tika;
 import org.apache.tika.detect.DefaultDetector;
 import org.apache.tika.detect.Detector;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.io.IOUtils;
+import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.langdetect.OptimaizeLangDetector;
 import org.apache.tika.language.LanguageIdentifier;
 import org.apache.tika.language.detect.LanguageDetector;
 import org.apache.tika.language.detect.LanguageHandler;
 import org.apache.tika.language.detect.LanguageResult;
 import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MediaType;
 import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.Parser;
@@ -52,7 +57,7 @@ public class Exercise2
             {
                 processFile(file);
             }
-        } catch (IOException | SAXException | TikaException e)
+        } catch (IOException | SAXException | TikaException | ParseException e)
         {
             e.printStackTrace();
         }
@@ -65,14 +70,9 @@ public class Exercise2
         langDetector = new OptimaizeLangDetector();
     }
 
-    private void processFile(File file) throws IOException, SAXException, TikaException
-    {
-
-        // TODO: extract content, metadata and language from given file
-        // call saveResult method to save the data
-
-
+    private void processFile(File file) throws IOException, SAXException, TikaException, ParseException {
         Parser parser = new AutoDetectParser();
+        MediaType mediaType;
         BodyContentHandler handler = new BodyContentHandler(-1);
         Metadata metadata = new Metadata();
         FileInputStream content = new FileInputStream(file);
@@ -80,9 +80,50 @@ public class Exercise2
         parser.parse(content,handler,metadata,new ParseContext());
         LanguageIdentifier li = new LanguageIdentifier(handler.toString());
         String languageCode = li.toString().substring(0,2);
-        
 
-        saveResult(file.getName(), languageCode, null, null, null, null, null); //TODO: fill with proper values
+        String author = null;
+        Date creationDate = null;
+        Date lastModificationDate = null;
+
+        ArrayList<String> authors = new ArrayList<>(Arrays.asList(metadata.getValues(Metadata.AUTHOR)));
+        ArrayList<String> createDates = new ArrayList<>(Arrays.asList(metadata.getValues(Metadata.CREATION_DATE)));
+        ArrayList<String> lastModificationDates = new ArrayList<>(Arrays.asList(metadata.getValues(Metadata.MODIFIED)));
+
+        Tika tika = new Tika();
+        tika.setMaxStringLength(-1);
+        String mimeType = tika.detect(file);
+
+
+        if (authors.size()>0){
+            author=authors.get(0);
+        }else{
+            author="";
+        }
+        if (createDates.size()>0) {
+            creationDate = new SimpleDateFormat(DATE_FORMAT).parse(createDates.get(0));
+        }
+        if (lastModificationDates.size()>0) {
+            lastModificationDate = new SimpleDateFormat(DATE_FORMAT).parse(lastModificationDates.get(0));
+        }
+
+
+        // TODO: 09.03.2020 How to write all file to String???
+        String finishContent = null;
+
+
+
+        saveResult(file.getName(), languageCode, author, creationDate, lastModificationDate, mimeType, finishContent);
+    }
+
+    private String getFileContent( FileInputStream fis ) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        Reader r = new InputStreamReader(fis, "UTF-8");  //or whatever encoding
+        int ch = r.read();
+        while(ch >= 0) {
+            sb.append(ch);
+            ch = r.read();
+        }
+        return sb.toString();
     }
 
     private void saveResult(String fileName, String language, String creatorName, Date creationDate,
